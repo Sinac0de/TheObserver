fe1ab087256e750ed15381b4e6c73719c30ebd15
Sina Moradian <sinacsharp@gmail.com>
2026-01-01 16:51:29 +0330

    feat(game): add singleton pattern and game states management
    - Implement singleton pattern for GameManager and GameInputManager classes
    - Add game states (MainMenu, Running, Paused, GameOver) in GameManager with events
    - Add input action handling with PlayerInputActions in GameInputManager
    - Expose move, look input and input events (Jump, Interact, Fire) in GameInputManager
    - Rename scene from SampleScene to Main
    - Add meta files for new asset folders Materials, Prefabs, ScriptableObjects

--- Assets/Scripts/Managers/GameInputManager.cs
+++ Assets/Scripts/Managers/GameInputManager.cs
@@ -1,16 +1,44 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
+using System;
 
-public class GameInputManager : MonoBehaviour
-{
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
+public class GameInputManager : MonoBehaviour {
+    public static GameInputManager Instance { get; private set; }
+
+    private PlayerInputActions playerInputActions;
+
+    public Vector2 MoveInput { get; private set; }
+    public Vector2 LookInput { get; private set; }
+
+    // Events
+    public event Action OnJump;
+    public event Action OnInteract;
+    public event Action OnFire;
+
+    private void Awake() {
+        if (Instance != null && Instance != this)
+            Destroy(gameObject);
+        else
+            Instance = this;
+
+        playerInputActions = new PlayerInputActions();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void OnEnable() {
+        playerInputActions.Player.Enable();
+
+        playerInputActions.Player.Move.performed += ctx => MoveInput = ctx.ReadValue<Vector2>();
+        playerInputActions.Player.Move.canceled += ctx => MoveInput = Vector2.zero;
+
+        playerInputActions.Player.Look.performed += ctx => LookInput = ctx.ReadValue<Vector2>();
+        playerInputActions.Player.Look.canceled += ctx => LookInput = Vector2.zero;
+
+        playerInputActions.Player.Jump.performed += ctx => OnJump?.Invoke();
+        playerInputActions.Player.Interact.performed += ctx => OnInteract?.Invoke();
+        playerInputActions.Player.Fire.performed += ctx => OnFire?.Invoke();
+    }
+
+    private void OnDisable() {
+        playerInputActions.Player.Disable();
     }
 }
--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,16 +1,47 @@
 using UnityEngine;
+using System;
 
-public class GameManager : MonoBehaviour
-{
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
+public class GameManager : MonoBehaviour {
+    public static GameManager Instance { get; private set; }
+    public GameState CurrentState { get; private set; }
+
+    public event Action<GameState> OnGameStateChanged;
+
+    public enum GameState {
+        MainMenu,
+        Running,
+        Paused,
+        GameOver
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+
+    private void Awake() {
+        if (Instance != null && Instance != this)
+            Destroy(gameObject);
+        else
+            Instance = this;
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void Start() {
+        SetState(GameState.Running);
+    }
+
+    public void SetState(GameState newState) {
+        CurrentState = newState;
+        OnGameStateChanged?.Invoke(newState);
+        Debug.Log("Game State changed to: " + newState);
+    }
+
+    public void ResetRun() {
+        // Reset player position, ammo, health
+        // Reset room manager if needed
+        Debug.Log("Run Reset");
+    }
+
+    public void GameOver() {
+        SetState(GameState.GameOver);
+        // Show UI, play sound
     }
 }
